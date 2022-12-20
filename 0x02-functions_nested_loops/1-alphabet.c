e89d6ff97913576b23c153f1bd7c418bc3ea45ea
Helena Nuwagaba <nhelena590@gmail.com>
2022-12-20 12:26:35 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,10 @@
 void print_alphabet(void)
 
 {
-	char c;
+	char letter;
 
-	for (c = 'a'; c <= 'g'; c++)
-	{
-	_putchar(c);
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 	_putchar('\n');
 }
