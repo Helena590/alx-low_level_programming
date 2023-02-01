56640b40991ad06c9d85c5026e58a9d36c5f9c7c
Helena590 <nhelena590@gmail.com>
2023-02-01 09:27:12 -0800

    task 4

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - deletes the head node
+ * @head: a double pointr
+
+ *
+ *     *Return: empty
+ *
+ *      */
+
+int pop_listint(listint_t **head)
